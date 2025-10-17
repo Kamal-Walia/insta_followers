61a61330bb4e2f77cf1380d62b15c8bd66ad0124
Kamal Walia <kamalwalia.dev@gmail.com>
2025-10-17 16:44:49 +0530

    Bug fix

--- insta.js
+++ insta.js
@@ -5,7 +5,7 @@ const followers = [];
   const simplifyJson = (json) => {
     if(json["relationships_following"]?.length){
       const updatedJson = json.relationships_following;
-      return updatedJson.map(item => item.string_list_data[0].value)
+      return updatedJson.map(item => item.title)
     }
     return json.map(item => item.string_list_data[0].value)
   }
@@ -13,3 +13,4 @@ const followers = [];
   const formatedFollowings = simplifyJson(following);
   const formatedFollowers = simplifyJson(followers);
   const notFollowing = formatedFollowings.filter(item => formatedFollowers.includes(item) ? false : item );
+  console.log("Not Following:", notFollowing);
